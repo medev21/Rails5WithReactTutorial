426e704e05a6315ec74d96cdc400c7df89bed7a1
Martin Ben <mebenav88@gmail.com>
2016-10-09 14:34:44 -0400

    add post

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -4,4 +4,19 @@ class LiftsController < ApplicationController
     @lifts = Lift.all
   end
 
+  def create
+    @lift = Lift.new(lift_params)
+
+    if @lift.save
+      render json: @lift
+    else
+      render json: @lift.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def lift_params
+    params.require(:lift).permit(:date, :liftname, :ismetric, :weightlifted, :repsperformed, :onerm)
+  end
+
 end
